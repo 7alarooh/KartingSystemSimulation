a95df259f3ce771e80d6b7b7bac6f1274222a94f
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-29 10:43:48 +0400

    Models and Relationships

---
+++ KartingSystemSimulation/Models/Race_Racer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartingSystemSimulation.Models
+{
+    public class RaceRacer
+    {
+        [Key]
+        public int RaceId { get; set; } // Foreign Key to Game
+        public int RacerId { get; set; } // Foreign Key to Racer
+
+        public Game Game { get; set; }
+        public Racer Racer { get; set; }
+    }
+
+}
---
+++ KartingSystemSimulation/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartingSystemSimulation.Models
+{
+    public class User
+    {
+        [Key]
+        public string LoginEmail { get; set; } // Primary Key
+        public string LoginPassword { get; set; } // Hashed Password
+        public string Role { get; set; } // ENUM: Admin, Racer, Supervisor
+    }
+}
