ef52dfab732adcfa725aecec8c12da0e52e0c4fd
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-12-30 13:08:00 +0400

    test to fix pull issue

---
+++ KartingSystemSimulation/DTOs/GameOutputDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KartingSystemSimulation.DTOs
+{
+    public class GameOutputDTO
+    {
+        [Required]
+        public int GameId { get; set; } // Unique identifier for the game
+
+        [Required]
+        [StringLength(50)]
+        public string RaceType { get; set; } // Type of the race
+
+        [Required]
+        [Range(1, 50)]
+        public int Laps { get; set; } // Total number of laps in the race
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime RaceDate { get; set; } // Date and time of the race
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int KartId { get; set; } // Assigned Kart ID for the game
+
+        [Required]
+        public List<string> RacerNames { get; set; } // List of racer full names
+
+        [Required]
+        public List<int> TopRacers { get; set; } // List of IDs for top racers
+    }
+}
