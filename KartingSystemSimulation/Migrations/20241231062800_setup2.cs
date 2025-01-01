56a48aa4cf6376706a015ebbdb5320ea83d628be
Zubair Al Bulushi <zubair.albu@gmial.com>
2025-01-01 12:01:33 +0400

    :(

---
+++ KartingSystemSimulation/Migrations/20241231062800_setup2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KartingSystemSimulation.Migrations
+{
+    /// <inheritdoc />
+    public partial class setup2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Membership",
+                table: "Racers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Membership",
+                table: "Racers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
