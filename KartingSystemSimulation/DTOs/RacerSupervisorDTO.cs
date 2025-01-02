92987233fe935d2daf5396a9336098cc9c51aa27
Zubair Al Bulushi <zubair.albu@gmial.com>
2025-01-02 09:40:29 +0400

    test

---
+++ KartingSystemSimulation/DTOs/RacerSupervisorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartingSystemSimulation.DTOs
+{
+    public class RacerSupervisorDTO
+    {
+        public string Name { get; set; } // Supervisor's name
+
+        public string CivilId { get; set; } // Unique Civil ID
+
+        public string Phone { get; set; } // Contact number
+
+        public string Email { get; set; } // Validated email
+    }
+}
